9346455b0f72f5d1d931e1f369b22b3936b872c6
muhamadansor05 <muhamadansor05@gmail.com>
2023-03-08 12:53:52 +0700

    update

---
+++ src/ANSOR070323/Latihan2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DINDA070323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class Latihan2 {
+    public static void main (String [] args){
+        int i = 10;
+        int j = 20;
+        int k = 45;
+        int average;
+        
+        average = (i+j+k)/3;
+       
+        System.out.println ("number 1 = "+i);
+        System.out.println("number 2 = " +j);
+        System.out.println("numbger 3 = " +k);
+        System.out.println("average is = " +average);
+        
+    }
+}
---
+++ src/ANSOR_2211081017/GetInputFromKeyboard.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ANSOR_2211081017;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class GetInputFromKeyboard {
+    public static void main(String[] args){
+        BufferedReader dataIn =
+                new BufferedReader(new InputStreamReader(System.in));
+        String name = "";
+        System.out.print("Please Enter Your Name : ");
+        try {
+            name = dataIn.readLine();
+        } catch(IOException e ){
+            System.out.println("Error!");
+        }
+        System.out.println("Hello " + name + "!");
+    }
+}
